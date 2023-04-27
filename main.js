f9ab29296482a8c9cceb6b96f90d96ca29996bd8
wzwolf <wzwolfx@gmail.com>
2023-04-27 02:12:50 +0000

    added more code to main.js, require debugging

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const path = require('path');
 
@@ -11,8 +10,8 @@ fileformat ='utf8';
  * returns template with flags replaced by text
  * @param {string} file path of text to replace
  * @param {string} file path of main text
- * @return {string} text of main text, with 
- * each [text] replaced with respective text 
+ * @return {string} text of main text, with
+ * each [text] replaced with respective text
  * as per text_to_replace file
 */
 function main(text_to_replace_file,maintext_file) {
@@ -20,17 +19,36 @@ function main(text_to_replace_file,maintext_file) {
 	// convert filepath to full file paths
 	const text_to_replace_full_path = path.join(__dirname,text_to_replace_file);
 	const maintext_full_path = path.join(__dirname,maintext_file);
-	// create list of text to replace. 
+	// create list of text to replace.
 	const text_to_replace = [];
 	// for each item in text_to_replace file
-	var filelines = [];
-	fs.readFile(text_to_replace_full_path,fileformat,(err,data)=>{
+	/*fs.readFile(text_to_replace_full_path,fileformat,(err,data)=>{
 		if(err){
 			console.error('error',err);
 			return;
 		}
-		console.log(data)
-	});
+		// convert raw string into object with search term and text to replace
+		console.log(data);
+		let filelines = data.split(/\r?\n/);
+		console.log(filelines);
+		filelines.forEach(function(line){
+			try{
+				let searcharray = line.split(/:/);
+				console.log(searcharray);
+				text_to_replace.push(searcharray);
+			} catch (err){}
+		});
+		console.log(text_to_replace);
+	})
+	console.log(text_to_replace,"testing");
+	*/
+	try{
+		const data = fs.readFileSync(text_to_replace_full_path,fileformat);
+		console.log(data);
+	} catch(err) {
+		console.log(err);
+	}
+
 }
 
 console.log(main(text_to_replace_filepath,maintext_filepath))
