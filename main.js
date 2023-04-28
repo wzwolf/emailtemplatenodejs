3736b09de7da4aa461169e38a60f63fc7d811867
wzwolf <wzwolfx@gmail.com>
2023-04-28 08:36:56 +0000

    finished code for replacing [text] with replacement text

--- main.js
+++ main.js
@@ -20,19 +20,42 @@ function main(text_to_replace_file,maintext_file) {
 	const text_to_replace_full_path = path.join(__dirname,text_to_replace_file);
 	const maintext_full_path = path.join(__dirname,maintext_file);
 	// get data from text to replace file
-	try{
-		var data = fs.readFileSync(text_to_replace_full_path,fileformat).split(/\r?\n/);
-		console.log("data : "+data);
-	} catch(err) {
-		console.log(err);
-	}
+	const data = extractdatafromfile(text_to_replace_full_path,fileformat);
 	console.log("data : "+data);
 	// convert data to array of search-replace-object
 	console.log("testing :" + new searchreplacepair("haha","hoho"));
 	const text_to_replace = [];
 	console.log(typeof data);
-	
+	var lines =data.split(/\r?\n/);
+	lines.forEach((line) => {
+		if(/:/.test(line)){
+			console.log(line);
+			let searcharray = line.split(/:/);
+			// trim whitespace, replace all [ with \[, replace all ] with /]
+			// eg. " [text] " to "\[text\]"
+			let searchkey = searcharray[0].trim()
+					.replace(/\[(.*?)\]/g,"\\[$1\\]");
+			console.log(searchkey);
+			let replacement = searcharray[1].trim();
+			let searchreplaceobj = new searchreplacepair(searchkey,replacement);
+			text_to_replace.push(searchreplaceobj);
+		}
+	});
+	console.log(text_to_replace);
+	// get main text
+	const maintext = extractdatafromfile(maintext_full_path,fileformat);
+	// for each searchreplacepair, replace search term with replacment in main.txt
+	var result = maintext;
+	for(const i in text_to_replace){
+		//console.log(text_to_replace[i]);
+		//let searchkey = text_to_replace[i].searchkey
+		let searchkey = new RegExp(text_to_replace[i].searchkey,"ig");
+		console.log(searchkey);
+		let replacement = text_to_replace[i].replacement;
+		result = result.replace(searchkey,replacement);
+	}
 	console.log('end of script');
+	return result;
 }
 
 /**
@@ -46,5 +69,21 @@ function searchreplacepair(searchkey,replacement) {
 	this.searchkey = searchkey;
 	this.replacement = replacement;
 }
+/**
+ * takes fullfilepath and return text inside the file
+ * @param {string} filepath
+ * @param {string} fileformat eg. utf8
+ * @return {string} text in file
+*/
+function extractdatafromfile(file_full_path,fileformat) {
+	try{
+		var output = fs.readFileSync(file_full_path,fileformat)
+	} catch(err) {
+		console.log(err);
+	}
+	//console.log(output);
+	return output;
+}
 
-console.log(main(text_to_replace_filepath,maintext_filepath))
+// main script run
+console.log(main(text_to_replace_filepath,maintext_filepath));
