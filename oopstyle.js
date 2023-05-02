5ab45e50893de0c51057873a0ff194cfa7d516d3
wzwolf <wzwolfx@gmail.com>
2023-05-02 10:12:49 +0000

    added file constructor

--- oopstyle.js
+++ oopstyle.js
@@ -2,10 +2,16 @@
 const fs  = require('fs');
 const path = require('path');
 
+//declare globals 
+text_to_replace_filepath = '/resources/text_to_replace.txt'; 
+maintext_filepath = '/resources/maintext.txt'; 
+fileformat ='utf8';
+
 // filedata class
 class file {
-	constructor(text_to_replace_file,main_text_file,format){
-		
+	constructor(text_file,format){
+		this.text_full_path = path.join(__dirname,text_file)
+		this.fileformat = format
 	}
 }
 
