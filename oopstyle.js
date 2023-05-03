d06920990eee88f7c8dabac4a1a803111d5393be
wzwolf <wzwolfx@gmail.com>
2023-05-03 06:13:40 +0000

    added oopstyle class and test script. current bug: failure to get error to throw when file does not exist

--- oopstyle.js
+++ oopstyle.js
@@ -1,17 +1,37 @@
-// this is a object oriented version of the same code 
+// this is a object oriented version of the same code
 const fs  = require('fs');
 const path = require('path');
 
-//declare globals 
-text_to_replace_filepath = '/resources/text_to_replace.txt'; 
-maintext_filepath = '/resources/maintext.txt'; 
+//declare globals
+text_to_replace_filepath = '/resources/text_to_replace.txt';
+maintext_filepath = '/resources/maintext.txt';
 fileformat ='utf8';
 
-// filedata class
-class file {
-	constructor(text_file,format){
-		this.text_full_path = path.join(__dirname,text_file)
-		this.fileformat = format
+// filedata object
+function filedata(text_file,fileformat) {
+	this.file = path.join(__dirname,text_file)
+	this.fileformat = fileformat
+}
+
+// attach function to filedata object
+// function to read data from file
+// return data from file;
+filedata.prototype.data= function() {
+	try{
+		if(!fs.existsSync(this.file)) {
+			console.log("File Not Found", this.file);
+			throw new Error("File Not Found");
+		} else {
+			var data = fs.readFileSync(this.file,this.fileformat);
+			console.log(data);
+			return data;
+		}
+	} catch(err) {
+		console.log(err);
+		return err;
 	}
 }
 
+module.exports = {
+	filedata
+	};
--- test/oopstyle.test.js
+++ test/oopstyle.test.js
@@ -1,28 +1,33 @@
-// import module to test
-const main = require("../oopstyle");
+const { filedata } = require('../oopstyle');
+const fs = require('fs');
 
-//console.log(main("../resources/text_to_replace.txt","../resources/maintext.txt")
-//console.log(typeof main);
-//console.log(main("test/searchreplace_1.txt","test/maintext_1.txt"));
-
-describe("main tests", () => {
-	// test 1 - standard working test
-	test("test1",() => {
-		//arrange and act
-		var result = main("test/searchreplace_1.txt","test/maintext_1.txt").trim;
-		// assert
-		expectedresult = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt test 1 replaced ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco test 3 replaced laboris nisi ut aliquip ex ea commodo consequat. test 2 replaced Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. test 1 replaced Excepteur sint occaecat cupidatat non proident, sunt test 3 replaced in culpa qui officia deserunt mollit anim id est laborum".trim;
-		expect(result).toBe(expectedresult);
+describe('filedata', () => {
+	const text_file = 'test/text_to_replace_oop_test1.txt';
+  	const fileformat = 'utf8';
+	const testContent = 'This is a test file.';
+	
+	beforeAll(() => {
+		// create a test file before each test
+		fs.writeFileSync(text_file, testContent, fileformat);
 	});
-	// test 2 - file not available error
-	test("failtest1",()=> {
-		//arrange and act 
-		const testFunction = () => main("", "");
-		// assert
-		expect(testFunction).toThrow(TypeError);
+
+	afterAll(() => {
+		// delete test file after each test
+		fs.unlinkSync(text_file)
 	});
-})
-
-
-
-
+	test('should read data from file', () => {
+		// Create a new filedata object and call the data() function
+		const myFileData = new filedata(text_file, fileformat);
+		const data = myFileData.data();
+		expect(data).toBe(testContent);
+	});
+  
+  	test('should throw an error when file is not found', () => {
+		// Create a filedata object with an invalid file path
+		const nonexistentFile = './resources/nonexistent.txt';    
+		const myFileData = new filedata(nonexistentFile, fileformat);
+		expect(() => {
+			myFileData.data();
+		}).toThrow();
+	});
+});
