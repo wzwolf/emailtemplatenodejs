85738ffd8f140841d03888a092430b1faf100fcb
wzwolf <wzwolfx@gmail.com>
2023-05-05 02:55:57 +0000

    updated READMe, debugging oopstyle.js

--- README.md
+++ README.md
@@ -6,8 +6,31 @@ nodemon module
 jest module for testing
 
 
-// temp solution while still figuring out how to auto run this in dockerfile without error
-run the following when settting up from a clean dockerfile
-chmod +x setup.sh
-./setup.sh
+how to install 
+build docker image from Dockerfile in dockerfile folder
 
+run docker image
+
+cd into home/emailtemplatenodejs
+
+
+how to use 
+
+add words to replace in resources/resources/text_to_replace.txt 
+format : 
+[replacethisword] : with this
+[anotherreplacement] : with this one too
+
+add main text to replace in resources/maintext.txt
+format:
+this is the word to replace [replacethisword].  heres another word to replace [anotherreplacement]. 
+
+npm start - run main.js code
+
+return string with main text modified with replacement text 
+
+npm oopstart - run oopstyle.js code // same version but written with objects
+
+return string with main text modified with replacement text
+
+unit test with npm test
--- test/oopstyle.test.js
+++ test/oopstyle.test.js
@@ -26,19 +26,7 @@ describe('filedata', () => {
 		//arrange and act
 		var result = main("test/searchreplace_1.txt","test/maintext_1.txt","utf8").trim;
 		// assert
-		var expectedresult = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt test 1 replaced 
-		ut labore et dolore magna aliqua. Ut enim 
-		ad minim veniam, quis nostrud 
-		exercitation ullamco test 3 replaced 
-		laboris nisi ut aliquip ex ea commodo 
-		consequat. test 2 replaced Duis aute 
-		irure dolor in reprehenderit in voluptate 
-		velit esse cillum dolore eu fugiat nulla 
-		pariatur. test 1 replaced Excepteur sint 
-		occaecat cupidatat non proident, sunt 
-		test 3 replaced in culpa qui officia 
-		deserunt mollit anim id est 
-		laborum".trim; 
+		var expectedresult = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt test 1 replaced ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco test 3 replaced laboris nisi ut aliquip ex ea commodo consequat. test 2 replaced Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. test 1 replaced Excepteur sint occaecat cupidatat non proident, sunt test 3 replaced in culpa qui officia deserunt mollit anim id est laborum".trim;
 		expect(result).toBe(expectedresult);
 	});
 	// test 2 - file not available error
